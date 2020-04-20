4653eed23f58b35b57a64d2bf29a766a97e90788
Vinckama <vincent.2000@orange.fr>
2020-04-20 12:13:14 +0200

    add data loader

---
+++ data/LoadData.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Vincent Roy @ Arthur Claude
+"""
+import numpy as np
+import pandas as pd
+from os import path
+
+
+def load_data(data_path="data"):
+    DowJones_pd = pd.read_csv(path.join(data_path, "DowJones.csv"),
+                              header=0, delimiter=",")
+    Nasdaq_pd = pd.read_csv(path.join(data_path, "Nasdaq.csv"),
+                              header=0, delimiter=",")
+    Russell2000_pd = pd.read_csv(path.join(data_path, "Russell2000.csv"),
+                              header=0, delimiter=",")
+    SP500_pd = pd.read_csv(path.join(data_path, "SP500.csv"),
+                              header=0, delimiter=",")
+
+    df_dict = {
+        "DowJones": DowJones_pd.Close,
+        "Nasdaq": Nasdaq_pd.Close,
+        "Russell2000": Russell2000_pd.Close,
+        "SP500": SP500_pd.Close
+    }
+
+    for (name, df) in df_dict.items():
+        df_dict[name] = np.asarray(df)
+
+    data_df = pd.DataFrame(df_dict, index = DowJones_pd.Date)
+    data_df.sort_index(inplace = True )
+    return data_df
---
+++ data/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Vincent Roy @ Arthur Claude
+"""
+
+
+from .LoadData import load_data
