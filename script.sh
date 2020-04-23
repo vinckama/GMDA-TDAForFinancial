8c9be228801091b44fc5ba7e605c4e611dedaa88
Vinckama <vincent.2000@orange.fr>
2020-04-24 01:57:13 +0200

    add a script as a demo of the package

---
+++ script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#pyconda='/Users/vincentroy/anaconda3/envs/GMDA-TDAForFinancial/bin/python'
+echo What is the location of your conda environnement?
+read pyconda
+
+# dataset
+(sleep 20s; echo '\r') | $pyconda manage.py dataset visualise
+echo '\n'
+(sleep 20s; echo '\r') | $pyconda manage.py dataset visualise --log
+echo '\n'
+
+# landscape
+(sleep 20s; echo '\r') | $pyconda manage.py landscape visualise -w_size=80 --end_date='2000-01-10'
+echo '\n'
+
+# norm
+$pyconda manage.py norm get -w_size=50
+echo '\n'
+(sleep 30s; echo '\r') | $pyconda manage.py norm visualise -w_size=50 --start_date='1997-01-03' --end_date='2000-05-10'
+echo '\n'
+(sleep 60s; echo '\r') | $pyconda manage.py norm crash_stats -w_size=50 -year=2008 --test --plot
+echo '\n'
+(sleep 60s; echo '\r') | $pyconda manage.py norm crash_stats -w_size=50 -year=2000 --test --plot
+echo '\n'
+
+# bottleneck
+$pyconda manage.py bottleneck get -w_size=50
+echo '\n'
+(sleep 30s; echo '\r') | $pyconda manage.py bottleneck visualise -w_size=80 --start_date='2005-01-03'
+--end_date='2008-09-30'
+echo '\n'
+(sleep 60s; echo '\r') | $pyconda manage.py bottleneck crash_stats -w_size=80 -year=2008 --test --plot
+echo '\n'
+(sleep 60s; echo '\r') | $pyconda manage.py bottleneck crash_stats -w_size=80 -year=2000 --test --plot
+echo '\n'
