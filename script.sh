56a8666260f55f9b9be0ce82813c1fb099fb472e
arthur-claude <arthur.claude98@gmail.com>
2020-04-24 18:18:36 +0200

    update script.sh

--- script.sh
+++ script.sh
@@ -10,26 +10,35 @@ echo ''
 echo ''
 
 # landscape
-(sleep 20s; echo '\r') | $pyconda manage.py landscape visualise -w_size=80 --end_date='2000-01-10'
+(sleep 20s; echo '\r') | $pyconda manage.py landscape visualise -w_size=50 --end_date='2000-01-10'
+echo ''
+(sleep 20s; echo '\r') | $pyconda manage.py landscape visualise -w_size=50 --end_date='2000-03-10'
+echo ''
+(sleep 20s; echo '\r') | $pyconda manage.py landscape visualise -w_size=50 --end_date='2008-07-15'
+echo ''
+(sleep 20s; echo '\r') | $pyconda manage.py landscape visualise -w_size=50 --end_date='2008-09-15'
 echo ''
 
 # norm
 $pyconda manage.py norm get -w_size=50
 echo ''
-(sleep 30s; echo '\r') | $pyconda manage.py norm visualise -w_size=50 --start_date='1997-01-03' --end_date='2000-05-10'
-echo ''
-(sleep 60s; echo '\r') | $pyconda manage.py norm crash_stats -w_size=50 -year=2008 --test --plot
+(sleep 30s; echo '\r') | $pyconda manage.py norm visualise -w_size=50 --start_date='1997-01-03' --end_date='2000-03-10'
 echo ''
 (sleep 60s; echo '\r') | $pyconda manage.py norm crash_stats -w_size=50 -year=2000 --test --plot
+echo ''
+(sleep 30s; echo '\r') | $pyconda manage.py norm visualise -w_size=50 --start_date='2005-06-08' --end_date='2008-09-15'
+echo ''
+(sleep 60s; echo '\r') | $pyconda manage.py norm crash_stats -w_size=50 -year=2008 --test --plot
 echo ''
 
 # bottleneck
 $pyconda manage.py bottleneck get -w_size=50
 echo ''
-(sleep 30s; echo '\r') | $pyconda manage.py bottleneck visualise -w_size=80 --start_date='2005-01-03'
---end_date='2008-09-30'
+(sleep 30s; echo '\r') | $pyconda manage.py bottleneck visualise -w_size=50 --start_date='1997-01-03' --end_date='2000-03-10'
+echo ''
+(sleep 60s; echo '\r') | $pyconda manage.py bottleneck crash_stats -w_size=50 -year=2000 --test --plot
+echo ''
+(sleep 30s; echo '\r') | $pyconda manage.py bottleneck visualise -w_size=50 --start_date='2005-06-08' --end_date='2008-09-15'
 echo ''
 (sleep 60s; echo '\r') | $pyconda manage.py bottleneck crash_stats -w_size=80 -year=2008 --test --plot
-echo ''
-(sleep 60s; echo '\r') | $pyconda manage.py bottleneck crash_stats -w_size=80 -year=2000 --test --plot
 echo ''
